0538b79b426c2ffe7e20251bcc91e083162be74e
emitsyk <emitsyk@gmail.com>
2025-04-13 01:04:47 +0700

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold text-blue-600">Привет, Vite + Tailwind!</h1>
+  </div>
+`;commit message
